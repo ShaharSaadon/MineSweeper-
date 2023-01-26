0aa29b459481ea0f9edc3d4c10906c8597b55c39
ShaharSaadon <123458593+ShaharSaadon@users.noreply.github.com>
2023-01-26 19:35:29 +0200

    Add files via upload

---
+++ Final Delivery - Thursday 2100/js/hint.js
@@ -0,0 +1,110 @@
+'use strict'
+
+var gIsHintMode
+var gLastHintnum
+var gIsMegaHint = 0
+var gMegaHintPositions = []
+
+function hint(hintNum) {
+    if (gIsHintMode) return
+    gIsHintMode = true
+    gLastHintnum = hintNum
+    var elHint = document.querySelector(`#hint${hintNum}`)
+    elHint.style.backgroundColor = 'yellow'
+}
+
+function hintMove(board, rowIdx, colIdx) {
+    var tempArr = []
+    var tempCell
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (j < 0 || j >= board[i].length) continue
+            tempCell = board[i][j].isShown
+            tempArr.push(tempCell)
+            board[i][j].isShown = true
+            renderBoard(board)
+        }
+    }
+    console.log(tempArr)
+    setTimeout(hintHide, 1000, board, rowIdx, colIdx, tempArr)
+    hintHide()
+}
+
+function hintHide(board, rowIdx, colIdx, tempArr) {
+    var isShownBefore
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (j < 0 || j >= board[i].length) continue
+            isShownBefore = tempArr.shift()
+            board[i][j].isShown = isShownBefore
+        }
+    }
+
+    renderBoard(gBoard)
+    gIsHintMode = false
+    console.log(`${gLastHintnum}`)
+    setTimeout(() => {
+        var elHint = document.querySelector(`#hint${gLastHintnum}`)
+        elHint.style.display = 'none'
+    }, 1000);
+
+
+}
+
+function megaHint() {
+    gIsMegaHint++
+    alert('You are In Mega-Hint move! Pay attention, you have this tool only once in a game. The first click is the upper left edge of the rectangle and the second click is the right and lower edge of the rectangle that will revealds for a second')
+}
+
+function onCellClickedMegaHint(elCell, i, j) {
+    gBoard[i][j].isShown = true
+    renderBoard(gBoard)
+    gMegaHintPositions.push({ i, j })
+    console.log(gMegaHintPositions)
+    gIsMegaHint++
+    if (gMegaHintPositions.length === 2) megaHintMove()
+}
+
+function megaHintMove() {
+    var tempArr = []
+    var tempCell
+
+    var firstRow = gMegaHintPositions[0].i
+    var firstCol = gMegaHintPositions[0].j
+    var lastRow = gMegaHintPositions[1].i
+    var lastCol = gMegaHintPositions[1].j
+    console.log(firstRow, firstCol + " aa", lastRow, lastCol)
+
+    for (var i = firstRow; i <= lastRow; i++) {
+        for (var j = firstCol; j <= lastCol; j++) {
+            tempCell = gBoard[i][j].isShown
+            tempArr.push(tempCell)
+            gBoard[i][j].isShown = true
+        }
+    }
+    renderBoard(gBoard)
+    setTimeout(megaHintHide, 1000, tempArr)
+
+}
+
+function megaHintHide(tempArr) {
+    var isShownBefore
+    var firstRow = gMegaHintPositions[0].i
+    var firstCol = gMegaHintPositions[0].j
+    var lastRow = gMegaHintPositions[1].i
+    var lastCol = gMegaHintPositions[1].j
+
+    for (var i = firstRow; i <= lastRow; i++) {
+        for (var j = firstCol; j <= lastCol; j++) {
+            isShownBefore = tempArr.shift()
+            gBoard[i][j].isShown = isShownBefore
+        }
+    }
+    gBoard[firstRow][firstCol].isShown=false
+    gBoard[lastRow][lastCol].isShown=false
+    renderBoard(gBoard)
+    gIsMegaHint=0
+
+}
---
+++ Final Delivery - Thursday 2100/js/manually.js
@@ -0,0 +1,40 @@
+function manuallyCreate() {
+    onInit()
+    var isUnderstand = confirm('You are going to manually create mode, which means you will decided where located the mines. Pay attention you are at the level that you want before. \n  \n Are you sure you want to get in Mannualy Mode?')
+    //i want to hide the other things on the page but later.
+    var elGameMode = document.querySelector('h2 span')
+    elGameMode.innerText = '!@#Manuallly mode!@#'
+    gGame.isManuallyMode = true
+}
+
+function onCellClickedManually(elCell, i, j) {
+
+    if (gGame.shownCount < gLevel.MINES) {
+        const cell = gBoard[i][j]
+        cell.isMine = true
+        cell.isShown = true
+        gGame.shownCount++
+        setMinesNegsCount(gBoard)
+        renderBoard(gBoard)
+
+    } else {
+        gIsFirstMove = false
+        gGame.isManuallyMode = false
+        for (var row = 0; row < gBoard.length; row++) {
+            for (var col = 0; col < gBoard[0].length; col++) {
+                if (gBoard[row][col].isShown) {
+                    gBoard[row][col].isShown = false
+                    gGame.shownCount--
+                    renderBoard(gBoard)
+
+                }
+            }
+        }
+        if (!gGame.isManuallyMode) {
+            gClickTime = Date.now()
+            gInterval = setInterval("timeUp(gClickTime)", 1000)
+            alert('now you can start playing')
+            onCellClicked(elCell, i, j)
+        }
+    }
+}
